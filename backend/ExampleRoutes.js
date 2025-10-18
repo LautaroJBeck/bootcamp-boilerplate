8266d83958ce65c82da273e79014d37c50c418c3
Lautaro Beck <lautarobeck.programmer@gmail.com>
2025-10-18 15:15:39 -0400

    Commit with the .evn

--- backend/ExampleRoutes.js
+++ backend/ExampleRoutes.js
@@ -1,61 +1,66 @@
-import express from 'express';
-import { getDb } from './ExampleConnect.js';
-import { ObjectId } from 'mongodb';
+import express from "express";
+import { getDb } from "./ExampleConnect.js";
+import { ObjectId } from "mongodb";
 
-let expressRouter = express.Router()
+let expressRouter = express.Router();
 
-expressRouter.route("/").get(async (request, response) =>{
-    let db = getDb()
-    let data = await db.collection("pets").find({}).toArray()
-    if (data.length > 0){
-        response.json(data)
-    }
-    else {
-        throw new Error("Notes data not found.")
-    }
-})
+expressRouter.route("/").get(async (request, response) => {
+  let db = getDb();
+  let data = await db.collection("pets").find({}).toArray();
+  if (data.length > 0) {
+    response.json(data);
+  } else {
+    console.log(response);
+    throw new Error("Notes data not found.");
+  }
+});
 
-expressRouter.route("/:id").get(async (request, response) =>{
-    let db = getDb()
-    let data = await db.collection("pets").findOne({_id: new ObjectId(request.params.id)})
-    if (Object.keys(data).length > 0){
-        response.json(data)
-    }
-    else {
-        throw new Error("Note data not found.")
-    }
-})
+expressRouter.route("/:id").get(async (request, response) => {
+  let db = getDb();
+  let data = await db
+    .collection("pets")
+    .findOne({ _id: new ObjectId(request.params.id) });
+  if (Object.keys(data).length > 0) {
+    response.json(data);
+  } else {
+    throw new Error("Note data not found.");
+  }
+});
 
 expressRouter.route("/").post(async (request, response) => {
-    let db = getDb()
-    let mongoObject = {
-        name: request.body.name,
-        breed: request.body.breed,
-        age: request.body.age,
-        url: request.body.url,
-    }
-    let data = await db.collection("pets").insertOne(mongoObject)
-    response.json(data)
-})
+  let db = getDb();
+  let mongoObject = {
+    name: request.body.name,
+    breed: request.body.breed,
+    age: request.body.age,
+    url: request.body.url,
+  };
+  let data = await db.collection("pets").insertOne(mongoObject);
+  response.json(data);
+});
 
 expressRouter.route("/:id").put(async (request, response) => {
-    let db = getDb()
-    let mongoObject = {
-        $set: {
-            name: request.body.name,
-            breed: request.body.breed,
-            age: request.body.age,
-            url: request.body.url,
-        }
-    }
-    let data = await db.collection("pets").updateOne({_id: new ObjectId(request.params.id)}, mongoObject)
-    response.json(data)
-})
+  let db = getDb();
+  let mongoObject = {
+    $set: {
+      name: request.body.name,
+      breed: request.body.breed,
+      age: request.body.age,
+      url: request.body.url,
+    },
+  };
+  let data = await db
+    .collection("pets")
+    .updateOne({ _id: new ObjectId(request.params.id) }, mongoObject);
+  response.json(data);
+});
 
 expressRouter.route("/:id").delete(async (request, response) => {
-    let db = getDb()
-    let data = await db.collection("pets").deleteOne({_id: new ObjectId(request.params.id)})
-    response.json(data)
-})
+  let db = getDb();
+  let data = await db
+    .collection("pets")
+    .deleteOne({ _id: new ObjectId(request.params.id) });
+  response.json(data);
+});
 
 export default expressRouter;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import { ThemeProvider, CssBaseline } from '@mui/material'
 import theme from './ExampleTheme.ts'
-import ExampleDashboardLocal from './ExampleDashboardLocal.tsx'
+import ExampleDashboard from './ExampleDashboard.tsx'
 // @ts-ignore
 import AboutUs from './AboutUs.jsx'
 // @ts-ignore
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Routes>
-        <Route path="/" element={<ExampleDashboardLocal />} />
+        <Route path="/" element={<ExampleDashboard />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/plan-visit" element={<PlanYourVisit />} />
       </Routes>
